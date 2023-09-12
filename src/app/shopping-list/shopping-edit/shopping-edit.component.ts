7c97fb988af6768b0d96c589650cb3ffb301adda
AbdullahAbuHaltam <abuhaltamabdullah@gmail.com>
2023-09-12 23:28:05 +0300

    Finished Edit Shopping list

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component,ElementRef,ViewChild,EventEmitter,Output } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent {
+@ViewChild('nameInput',{static:false}) name:ElementRef;
+@ViewChild('amountInput',{static:false}) amount:ElementRef;
+@Output() ingredientAdded=new EventEmitter<Ingredient>();
 
+
+  onAddItem(){
+    const newIngredient=new Ingredient(this.name.nativeElement.value,this.amount.nativeElement.value);
+    this.ingredientAdded.emit(newIngredient);
+  }
 }
