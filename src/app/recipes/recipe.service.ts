af1f83ebd9402ef6631fa10c414be97ffd81b86f
AbdullahAbuHaltam <abuhaltamabdullah@gmail.com>
2023-09-15 14:49:35 +0300

    Finished Recipe Service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,9 @@
 import { Recipe } from "./recipe.model";
+import { EventEmitter } from '@angular/core';
+
 
 export class RecipeService{
+    recipeSelected=new EventEmitter<Recipe>();
     private recipes :Recipe[]=[
         new Recipe('A Test Recipe 1','This is a simply a test','https://realfood.tesco.com/media/images/1400x919-HuntersChicken-1cddb648-ead8-4325-8e60-f80d1ca4ede3-0-1400x919.jpg'),
         new Recipe('A Test Recipe 2','This is a simply a test','https://realfood.tesco.com/media/images/1400x919-HuntersChicken-1cddb648-ead8-4325-8e60-f80d1ca4ede3-0-1400x919.jpg'),
@@ -9,4 +12,7 @@ export class RecipeService{
     getRecipe(){
         return this.recipes.slice();
     }
+
+    
+
 }
