e9f116ff7bb4938b80ccbd7b4da824b293d75576
AbdullahAbuHaltam <abuhaltamabdullah@gmail.com>
2023-09-20 15:09:19 +0300

    Added Observable

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,10 @@
-import { Subject } from "rxjs";
 import { Ingredient } from "../shared/ingredient.model";
 import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { Recipe } from "./recipe.model";
-import { EventEmitter,Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 
 @Injectable()
 export class RecipeService{
-    recipeSelected=new Subject<Recipe>();
     private recipes :Recipe[]=[
         new Recipe('A Test Recipe 1',
         'This is a simply a test',
